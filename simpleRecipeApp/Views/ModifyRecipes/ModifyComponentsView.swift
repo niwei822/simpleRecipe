3268c44b665dda3eb6c9e980ea9db9a459fce8b9
niwei822 <cecily.li.822@gmail.com>
2022-10-12 21:16:14 -0700

    add move and delete feature

--- simpleRecipeApp/Views/ModifyRecipes/ModifyComponentsView.swift
+++ simpleRecipeApp/Views/ModifyRecipes/ModifyComponentsView.swift
@@ -43,11 +43,23 @@ struct ModifyComponentsView<Component: RecipeComponent, DestinationView: ModifyC
                         .font(.title)
                         .padding()
                     Spacer()
+                    EditButton()
+                        .padding()
                 }
                 List {
                     ForEach(components.indices, id: \.self) { index in
                         let component = components[index]
-                        Text(component.description)
+                        let editComponentView = DestinationView(component: $components[index]) { _ in
+                            return
+                        }
+                            .navigationTitle("Edit " + "\(Component.singularName().capitalized)")
+                        NavigationLink(component.description,
+                        destination: editComponentView)
+                    }
+                    .onDelete { components.remove(atOffsets: $0) }
+                    //moves any selected element from its old index to its now index.
+                    .onMove{ indices, newOffset in
+                        components.move(fromOffsets: indices, toOffset: newOffset)
                     }
                     .listRowBackground(listBackgroundColor)
                     NavigationLink("Add another \(Component.singularName())",
